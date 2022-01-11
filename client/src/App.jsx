174c457c0804ba55f78eaa7d2c1c1f3d772d0740
Pinaki Bhattacharjee <pinakipb2@gmail.com>
2022-01-11 18:00:26 +0530

    updated versions

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,19 @@
-import React, { useState, useEffect } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
-import getWeb3 from "./getWeb3";
+import React, { useState, useEffect } from 'react';
+import SimpleStorageContract from './contracts/SimpleStorage.json';
+import getWeb3 from './getWeb3';
 
-import "./App.css";
+import './App.css';
 
 const App = () => {
-  
-  const [account, setAccount] = useState(null)
-  const [contract, setContract] = useState(null)
-  const [storageValue, setstorageValue] = useState(0)
-  
+  const [account, setAccount] = useState(null);
+  const [contract, setContract] = useState(null);
+  const [storageValue, setstorageValue] = useState(0);
+
   useEffect(() => {
     initial();
-  }, [])
-  
-  const initial = async() => {
+  }, []);
+
+  const initial = async () => {
     try {
       const web3 = await getWeb3();
       const accounts = await web3.eth.getAccounts();
@@ -25,31 +24,26 @@ const App = () => {
       setContract(instance);
       await instance.methods.set(5).send({ from: accounts[0] });
       const response = await instance.methods.get().call();
-      setstorageValue(response)
+      setstorageValue(response);
     } catch (error) {
       // Catch any errors for any of the above operations.
-      alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`,
-      );
+      alert(`Failed to load web3, accounts, or contract. Check console for details.`);
       console.error(error);
     }
-  }
-  
+  };
+
   return (
     <div className="App">
       <h1>Good to Go!</h1>
       <p>Your Truffle Box is installed and ready.</p>
       <h2>Smart Contract Example</h2>
+      <p>If your contracts compiled and migrated successfully, below will show a stored value of 5 (by default).</p>
       <p>
-        If your contracts compiled and migrated successfully, below will show
-        a stored value of 5 (by default).
-      </p>
-      <p>
-        Try changing the value stored on <strong>line 42</strong> of App.js.
+        Try changing the value stored on <strong>line 44</strong> of App.js.
       </p>
       <div>The stored value is: {storageValue}</div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- truffle-config.js
+++ truffle-config.js
@@ -1,17 +1,22 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
-  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+  contracts_build_directory: path.join(__dirname, 'client/src/contracts'),
   networks: {
     develop: {
-      port: 7545
-    }
+      port: 7545,
+    },
+    test: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*',
+    },
   },
   compilers: {
     solc: {
-      version: "^0.8.0"
-    }
-  }
+      version: '0.8.10',
+    },
+  },
 };
